164d2986f214473c54100492fce5cd116bcd9da7
Zloybin <zlobinyevhen0707@gmail.com>
2026-06-25 15:36:43 +0200

    Added TaskService class.

--- src/main/java/com/arduino/telegrambot/service/TaskService.java
+++ src/main/java/com/arduino/telegrambot/service/TaskService.java
@@ -1,4 +1,29 @@
 package com.arduino.telegrambot.service;
 
+import com.arduino.telegrambot.enummeration.NumberSystem;
+import org.jvnet.hk2.annotations.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class TaskService {
+    private Random random = new Random();
+
+    public String generateTask() {
+
+        var values = List.of(NumberSystem.values());
+        int sourceRandomIndex = random.nextInt(values.size() - 1);
+        var sourceSys = values.get(sourceRandomIndex);
+
+        values.remove(sourceRandomIndex);
+
+        int drainRandomSys = random.nextInt(values.size() - 1);
+        var drainSys = values.get(drainRandomSys);
+
+        int task = random.nextInt(129);
+        return sourceSys.name() + drainSys.name() + task;
+    }
+
+
 }

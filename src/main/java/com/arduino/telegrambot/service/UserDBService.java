0342d31f7b7e8bd814ceec7bf4844abc5cf48283
Zloybin <zlobinyevhen0707@gmail.com>
2026-06-25 15:33:42 +0200

    Added UserDBService class

--- src/main/java/com/arduino/telegrambot/service/UserDBService.java
+++ src/main/java/com/arduino/telegrambot/service/UserDBService.java
@@ -1,25 +1,60 @@
 package com.arduino.telegrambot.service;
 
-import com.arduino.telegrambot.keyboard.MenuType;
+import com.arduino.telegrambot.enummeration.UserState;
+import com.arduino.telegrambot.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-public class UserStateService {
+public class UserDBService {
 
-    private final Map<Long, MenuType> userStates = new ConcurrentHashMap<>();
+    private static final Map<Long, User> userDataBase = new ConcurrentHashMap<>();
 
-    public MenuType getUserState(Long chatId) {
-        return userStates.getOrDefault(chatId, MenuType.MAIN);
+    // User
+
+    public void putUser(Long chatId, User user) {
+        userDataBase.put(chatId, user);
     }
 
-    public void setUserState(Long chatId, MenuType menuType) {
-        userStates.put(chatId, menuType);
+    public User getUser(Long chatId) {
+        return userDataBase.get(chatId);
     }
 
-    public void resetUserState(Long chatId) {
-        userStates.remove(chatId);
+    public User getOrDefault(Long chatId) {
+        var user = getUser(chatId);
+        return user == null ? buildAndPutDefaultUser(chatId) : user;
+    }
+
+
+    // User state
+
+    public void setUserState(Long chatId, UserState userState) {
+        var user = getUser(chatId);
+        user.setState(userState);
+    }
+
+    public UserState getUserState(Long chatId) {
+        if (userDataBase.get(chatId) == null) {
+            throw new IllegalArgumentException(String.format("Нет пользователя с таким id: %s", chatId));
+        }
+        return userDataBase.get(chatId).getState();
+    }
+
+
+    // Default user
+
+    private static User buildAndPutDefaultUser(Long chatId) {
+        var deafaultUser = buildDefaultUser(chatId);
+        userDataBase.put(chatId, deafaultUser);
+        return deafaultUser;
+    }
+
+    private static User buildDefaultUser(Long chatId) {
+        return User.builder()
+                .id(chatId)
+                .state(UserState.FREE)
+                .build();
     }
 }

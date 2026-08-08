e7511f6c0b9e856918fcb47b1fbf750578e5eea7
Zloybin <zlobinyevhen0707@gmail.com>
2026-08-08 14:50:18 +0200

    Редакатирование сущности для маппинга. Подключение к БД с автоматическим созданием таблицы на основе сущности.

--- src/main/java/com/arduino/telegrambot/model/Task.java
+++ src/main/java/com/arduino/telegrambot/model/Task.java
@@ -2,18 +2,48 @@ package com.arduino.telegrambot.model;
 
 import com.arduino.telegrambot.enummeration.AnswerType;
 import com.arduino.telegrambot.enummeration.TaskLevel;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "task_number", nullable = false)
     private int taskNumber;
+
+    @Column(name = "self_task_number", nullable = false)
     private int selfTaskNumber;
+
+    @Column(name = "task_level", nullable = false)
+    @Enumerated(EnumType.STRING)
     private TaskLevel taskLevel;
+
+    @Column(name = "task_letter", nullable = false)
     private char taskLetter;
+
+    @Column(name = "has_image", nullable = false)
     private boolean hasImage;
+
+    @Column(name = "page_number", nullable = false)
     private int pageNumber;
+
+    @Column(name = "answer_type", nullable = false)
+    @Enumerated(EnumType.STRING)
     private AnswerType answerType;
+
+    @Column(name = "answer", nullable = false)
     private String answer;
+
+    @Column(name = "page_link", nullable = false)
     private String pageLink;
+
+    @Column(name = "task_text", nullable = false)
     private String taskText;
 }

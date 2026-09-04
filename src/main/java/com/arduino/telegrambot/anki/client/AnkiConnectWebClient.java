66f58d0f4fbdda40305c2d81c7e669ec4df713d8
Zloybin <zlobinyevhen0707@gmail.com>
2026-09-04 18:15:42 +0200

    постройка меню для Youglish.1

--- src/main/java/com/arduino/telegrambot/anki/client/AnkiConnectWebClient.java
+++ src/main/java/com/arduino/telegrambot/anki/client/AnkiConnectWebClient.java
@@ -24,7 +24,8 @@ public class AnkiConnectWebClient implements AnkiConnectClient {
 
     public AnkiConnectWebClient(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder
-                .baseUrl("http://127.0.0.1:8765")
+//                .baseUrl("http://127.0.0.1:8765")
+                .baseUrl("http://host.docker.internal:8765")
                 .build();
     }
 

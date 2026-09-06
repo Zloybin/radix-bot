3948e0cb1c8938f0b75cec4ea74f2804bcc5d0c3
Zloybin <zlobinyevhen0707@gmail.com>
2026-09-06 19:02:34 +0200

    Фикс подключения к Telegram bot'у из контейнера

--- src/main/java/com/arduino/telegrambot/TelegramBotService.java
+++ src/main/java/com/arduino/telegrambot/TelegramBotService.java
@@ -5,17 +5,12 @@ import com.arduino.telegrambot.entity.User;
 import com.arduino.telegrambot.enummeration.UserState;
 import com.arduino.telegrambot.model.UserRequest;
 import com.arduino.telegrambot.properties.AppProperties;
-import com.arduino.telegrambot.repository.UserRepository;
 import com.arduino.telegrambot.service.UserService;
-import com.arduino.telegrambot.youglish.YouglishService;
-import com.arduino.telegrambot.youglish.model.YouglishResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.util.List;
 
 @Component
 @Slf4j
